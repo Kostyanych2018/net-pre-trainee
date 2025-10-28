af687f68903ccb0a615310e6fcb608e691f231c2
Konstantin <kostyanych2018@gmail.com>
2025-10-28 23:30:35 +0300

    Indentation fix

--- Week4/Task4.Domain/Entities/Author.cs
+++ Week4/Task4.Domain/Entities/Author.cs
@@ -6,10 +6,7 @@ namespace Task4.Domain.Entities;
 public class Author
 {
     public int Id { get; set; }
-
     public string Name { get; set; }
-
     public DateTimeOffset DateOfBirth { get; set; }
-
     public ICollection<Book> Books { get; set; } = [];
 }
--- Week4/Task4.Domain/Entities/Book.cs
+++ Week4/Task4.Domain/Entities/Book.cs
@@ -1,12 +1,9 @@
-
 namespace Task4.Domain.Entities;
 
 public class Book
 {
     public int Id { get; set; }
-    
     public string Title { get; set; }
     public int PublishedYear { get; set; }
     public int? AuthorId { get; set; }
-    
 }
